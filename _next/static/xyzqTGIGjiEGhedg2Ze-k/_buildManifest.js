391312c428588470ab757087d7e2052c73e18cf8
NinePiece2 <NinePiece2@users.noreply.github.com>
2025-08-25 05:46:51 +0000

    deploy: 62d4468a318501985a28288205c63dcd9d1c163c

---
+++ _next/static/xyzqTGIGjiEGhedg2Ze-k/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/d4930bf8bb296eb9.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/TMetAeroWebsiteDev//_next/:path+",
+        "destination": "/TMetAeroWebsiteDev/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
